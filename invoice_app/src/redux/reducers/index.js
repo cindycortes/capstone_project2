a0f279648f10344ae288db417800511c1eaab12b
Cindy Cortes <cindy.indins@gmail.com>
2019-03-26 12:55:30 -0700

    redux-form

--- invoice_app/src/redux/reducers/index.js
+++ invoice_app/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import invoices from './invoicesReducer';
 // import businesses from './businessesReducer';
 // import drivers from './driversReducer';
 
 export default combineReducers({
     invoices, 
+    form: formReducer
     // businesses,
     // drivers
 })
