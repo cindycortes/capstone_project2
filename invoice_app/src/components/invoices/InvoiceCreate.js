3f186f0adda1888434caedae2e2a91856e9caf40
Cindy Cortes <cindy.indins@gmail.com>
2019-03-27 13:14:30 -0700

    Edit to Add Invoice form

--- invoice_app/src/components/invoices/InvoiceCreate.js
+++ invoice_app/src/components/invoices/InvoiceCreate.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
-import { createInvoice } from '../../redux/actions/invoiceAction';
-import { Form, FormGroup, Label } from 'reactstrap'; 
+// import { createInvoice } from '../../redux/actions/invoiceAction';
+import { Form, FormGroup, Label, Row, Col } from 'reactstrap';
 
 
 class InvoiceCreate extends React.Component {
@@ -33,68 +33,97 @@ class InvoiceCreate extends React.Component {
     //     )
     // }
 
-    onSubmit = (formValues) => {
-        // console.log(formValues);
-        this.props.createInvoice(formValues);
+    submit = values => {
+        console.log(values)
     }
 
+    
     render() {
+        
         // console.log(this.props);
         return (
-            <Form onSubmit={this.props.handleSubmit(this.onSubmit)} >
-                <FormGroup>
-                    <Label for="business">Business</Label>
-                    <Field className="form-control" name="business" component="input" label="Business" />
+            <Form className="container" onSubmit={this.submit}>
+                <h1>Add New Invoice </h1>
+                <Row>
+                    <Col md={6}>
+                        <FormGroup>
+                            <Label for="business">Business Name</Label>
+                            <Field className="form-control" name="business" component="input" type="text" placeholder="Business Name" />
+
+                        </FormGroup>
+                    </Col>
+                    <Col md={3}>
+                        <FormGroup>
+                            <Label for="date">Date</Label>
+                            <Field className="form-control" name="date" component="input" type="text" label="date" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={3}>
+
+                        <FormGroup>
+                            <Label for="invoiceNumber">Invoice No.</Label>
+                            <Field className="form-control" name="invoiceNumber" component="input" type="text" label="invoiceNumber" />
+                        </FormGroup>
+                    </Col>
+
+                </Row>
+
+                <Row>
                     
-                </FormGroup>
-                
-                <FormGroup>
-                    <Label for="invoiceNumber">Invoice No.</Label>
-                    <Field className="form-control" name="invoiceNumber" component="input" label="invoiceNumber" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="date">Date</Label>
-                    <Field className="form-control" name="date" component="input" label="date" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="shipper">Shipper</Label>
-                    <Field className="form-control" name="shipper" component="input" label="shipper" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="consignee">Consignee</Label>
-                    <Field className="form-control" name="consignee" component="input" label="consignee" />
-                </FormGroup>
+                    <Col md={6}>
+                        <FormGroup>
+                            <Label for="shipper">Shipper</Label>
+                            <Field className="form-control" name="shipper" component="input" type="text" label="shipper" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={6}>
+                        <FormGroup>
+                            <Label for="consignee">Consignee</Label>
+                            <Field className="form-control" name="consignee" component="input" type="text" label="consignee" />
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                    </Col>
+                </Row>
+
+
                 <FormGroup>
                     <Label for="dateShipped">Date Shipped: </Label>
-                    <Field className="form-control" name="dateShipped" component="input" label="dateShipped" />
+                    <Field className="form-control" name="dateShipped" component="input" type="text" label="dateShipped" />
                 </FormGroup>
                 <FormGroup>
                     <Label for="driver">Driver </Label>
-                    <Field className="form-control" name="driver" component="input" label="driver" />
+                    <Field className="form-control" name="driver" component="input" type="text" label="driver" />
                 </FormGroup>
                 <FormGroup>
                     <Label for="shippersNumbers">Shippers No. : </Label>
-                    <Field className="form-control" name="shippersNumbers" component="input" label="shippersNumbers" />
+                    <Field className="form-control" name="shippersNumbers" component="input" type="text" label="shippersNumbers" />
                 </FormGroup>
                 <FormGroup>
                     <Label for="description_commodity">Description_Commodity </Label>
-                    <Field className="form-control" name="description_commodity" component="input" label="description_commodity" />
+                    <Field className="form-control" name="description_commodity" component="input" type="text" label="description_commodity" />
                 </FormGroup>
                 <FormGroup>
                     <Label for="weight_quantity">Description_Commodity </Label>
-                    <Field className="form-control" name="weight_quantity" component="input" label="weight_quantity" />
+                    <Field className="form-control" name="weight_quantity" component="input" type="text" label="weight_quantity" />
                 </FormGroup>
                 <FormGroup>
                     <Label for="rate">Description_Commodity </Label>
-                    <Field className="form-control" name="rate" component="input" label="rate" />
+                    <Field className="form-control" name="rate" component="input" type="text" label="rate" />
                 </FormGroup>
                 <FormGroup>
                     <Label for="charges">Description_Commodity </Label>
-                    <Field className="form-control" name="charges" component="input" label="charges" />
+                    <Field className="form-control" name="charges" component="input" type="text" label="charges" />
                 </FormGroup>
-               
 
-                <button>Submit</button>
+
+                <button type="submit">Submit</button>
             </Form>
         )
     }
@@ -103,9 +132,7 @@ class InvoiceCreate extends React.Component {
 
 //initial values
 
-const formWrapped = reduxForm({
-    form: 'invoiceCreate'
-  
-})(InvoiceCreate);
 
-export default InvoiceCreate;
+export default reduxForm({
+    form: 'invoiceCreate', 
+}) (InvoiceCreate)
